868ea992c2f475b18ea17a0c90ecd07ecec986aa
Bhavin <bhulani@gmail.com>
2018-07-08 01:15:43 +0000

    Added publish waypoints

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -72,10 +72,18 @@ class WaypointUpdater(object):
         pos_vect = np.array([x, y])
         
         val = np.dot(cl_vect - prev_vect, pos_vect - cl_vect)
+        # If waypoint is behind us take the next one
         if val > 0
             closest_idx = (closest_idx + 1) % len(self.waypoints_2d)
             
         return closest_idx
+    
+    def publish_waypoints(self, closest_idx)
+        lane = Lane()
+        lane.header = self.base_waypoints.header
+        lane.waypoints = self.base_waypoints.waypoints[closest_idx: closest_idx + LOOKAHEAD_WPS]
+        self.final_waypoints_pub.publish(lane)
+        
     def pose_cb(self, msg):
         self.pose = msg
 
