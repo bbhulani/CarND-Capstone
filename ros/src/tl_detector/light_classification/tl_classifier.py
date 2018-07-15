eca7fe14ceb4ddc20bcdfbd8223f68034536d44c
N. Ben Cohen <breathevalue@icloud.com>
2018-07-15 16:29:22 -0700

    squash

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -10,6 +10,8 @@ from .train_classifier import CLASS_LABELS, IMAGE_SIZE
 import numpy as np
 import cv2
 
+import os
+
 model_name = "light_classification/tl_classifier_00.hdf5"
 
 
@@ -18,22 +20,24 @@ class TLClassifier(object):
 
         self.class_label_to_state_as_int32 = class_label_to_state_as_int32
 
-        # check that model Keras version is same as local Keras version
-        f = h5py.File(model_name, mode='r')
-        model_version = f.attrs.get('keras_version')
-        keras_version = str(keras.__version__).encode('utf8')
+        if os.path.exists(model_name):
+            # check that model Keras version is same as local Keras version
+            f = h5py.File(model_name, mode='r')
+            model_version = f.attrs.get('keras_version')
+            keras_version = str(keras.__version__).encode('utf8')
 
-        if model_version != keras_version:
-            print('You are using Keras version ', keras_version,
-                  ', but the model was built using ', model_version)
+            if model_version != keras_version:
+                print('You are using Keras version ', keras_version,
+                      ', but the model was built using ', model_version)
 
-        # not sure why this is needed -- workaround for issue described here ... https://github.com/keras-team/keras/issues/6462
-        self.graph = tf.get_default_graph()
+            # not sure why this is needed -- workaround for issue described here ... https://github.com/keras-team/keras/issues/6462
+            self.graph = tf.get_default_graph()
 
-        self.model = keras.models.load_model(model_name)
+            self.model = keras.models.load_model(model_name)
 
-        # do this in advance
-        self.model._make_predict_function()
+            # do this in advance
+            self.model._make_predict_function()
+        self.model = None
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
